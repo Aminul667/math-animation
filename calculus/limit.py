6ea9af9a050c6f19f2b3f8bcd1b85ddc1d244eac
aminul <rahathssn@gmail.com>
2024-09-18 01:18:25 +0600

    added definition of limit

--- calculus/limit.py
+++ calculus/limit.py
@@ -1,8 +1,13 @@
 from manim import *
 
 
-class CreateCircle(Scene):
+class Limit1(Scene):
     def construct(self):
-        circle = Circle()  # create a circle
-        circle.set_fill(PINK, opacity=0.5)  # set the color and transparency
-        self.play(Create(circle))
+        bangla = Text(
+            "In calculus, we apply operations to functions, producing new functions as a result.",
+            font="Agency FB",
+            font_size=25,
+            t2c={'operations to functions': BLUE, 'new functions': BLUE}
+        ).to_edge(UP)
+        self.play(Write(bangla))
+        self.wait()
